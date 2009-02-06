9c38cf504d09ffa36e6f62613b6a8a4508466a54
zuwiki <zuwiki@gmail.com>
2009-02-05 20:46:16 -0800

    Working on handler

--- dove/modules/user.py
+++ dove/modules/user.py
@@ -1,15 +1,13 @@
 from dove.protocol.rpc import DoveService, ServiceMethod
+
+@ServiceMethod
+def create(self, user={}):
+    pass
  
-class user(DoveService):
+@ServiceMethod
+def update(self, user={}):
+    pass
  
-    @ServiceMethod
-    def create(self, user={}):
-        pass
- 
-    @ServiceMethod
-    def update(self, user={}):
-        pass
- 
-    @ServiceMethod
-    def delete(self, uid=None):
-        pass
+@ServiceMethod
+def delete(self, uid=None):
+    pass
