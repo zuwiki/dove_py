e8e095d5e3dc711b1541e42ebf52f330f8ab435f
jmkogut <joshua.kogut@gmail.com>
2009-02-09 21:35:28 -0800

    Added a quick harness to test the transport and handler

---
+++ test/transport.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('..')
+
+from dove.protocol.transport import Transport, SocketHandler
+
+if __name__ == '__main__':
+    address = ('0.0.0.0', 4644)
+    t = Transport(address, SocketHandler)
+    t.debug = True
+    t.serve_forever()
